50b118688f008b32eb346da5e176a116e1503fbc
DFirsa <mr.danis2000@gmail.com>
2018-11-22 00:25:19 +0300

    5 lab

--- src/com/company/lab5/Map.java
+++ src/com/company/lab5/Map.java
@@ -1,4 +1,150 @@
 package com.company.lab5;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Map {
+
+    static PrintWriter out;
+
+    static int hash(String s, int tableSize){
+        int str = 0;
+        for (int i = 0; i < s.length(); i++){
+            str = 31 * str + s.charAt(i);
+        }
+        str = Math.abs(str % tableSize);
+        return str;
+    }
+
+    static class ListElement {
+        ListElement next;
+        ListElement prev;
+        String key;
+        String data;
+    }
+
+    static class List{
+        ListElement head;
+        ListElement tail;
+
+        void put(String key, String value){
+            ListElement a = new ListElement();
+            a.data = value;
+            a.key = key;
+            if (tail == null){
+                head = a;
+                tail = a;
+            }
+            else{
+                ListElement i;
+                i = head;
+                while (i != null){
+                    if (i.key.compareTo(key) == 0){
+                        i.data = value;
+                        return;
+                    }
+                    i = i.next;
+                }
+
+                tail.next = a;
+                a.prev =  tail;
+                tail = a;
+            }
+        }
+
+        void delete(String key){
+            ListElement a;
+            a = head;
+            while (a != null){
+                if (a.key.compareTo(key) == 0){
+                    if (a.next == null && a.prev == null){
+                        tail = null;
+                        head = null;
+                        return;
+                    }
+                    else{
+                        if (a.next == null){
+                            tail = a.prev;
+                            a.prev.next = null;
+                            return;
+                        }
+                        else{
+
+                            if (a.prev == null){
+                                a.next.prev = null;
+                                head = a.next;
+                                return;
+                            }
+                            else {
+                                a.next.prev = a.prev;
+                                a.prev.next = a.next;
+                                return;
+                            }
+                        }
+                    }
+                }
+                else {
+                    a = a.next;
+                }
+            }
+        }
+
+        void get(String key){
+            ListElement a = head;
+            while (a != null){
+                if (key.compareTo(a.key) == 0) {
+                    out.println(a.data);
+                    return;
+                }
+                a = a.next;
+            }
+            out.println("none");
+        }
+
+//        void print(){
+//            ListElement i = head;
+//            while (i != null) {
+//                System.out.print(i.data + " ");
+//                i = i.next;
+//            }
+//            System.out.println();
+//        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+//        Scanner in = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new FileReader(new File("map.in")));
+        out = new PrintWriter("map.out");
+
+        List[] map = new List[1 << 21];
+        for (int i = 0; i < map.length; i++) map[i] = new List();
+
+        String comand, key, value, line;
+
+        int h;
+
+        while ((line = reader.readLine()) != null){
+            String[] in = line.split(" ");
+            comand = in[0];
+            key = in[1];
+            h = hash(key, map.length);
+
+            if (comand.compareTo("put") == 0){
+                value = in[2];
+                map[h].put(key, value);
+            }
+
+            if (comand.compareTo("get") == 0) map[h].get(key);
+
+            if (comand.compareTo("delete") == 0) map[h].delete(key);
+
+//            if (comand.compareTo("end") == 0) break;
+//
+//            if (comand.compareTo("print") == 0) map[h].print();
+        }
+
+        reader.close();
+        out.close();
+    }
 }
--- src/com/company/lab5/Set.java
+++ src/com/company/lab5/Set.java
@@ -1,102 +1,123 @@
 package com.company.lab5;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Set {
 
     static PrintWriter out;
 
-    static int hash1(int n, int tableSize){
-        int hash = Math.abs(n % tableSize);
-        return hash;
+    static int hash(int x, int tableSize){
+        x = ((x >> 16) ^ x) * 0x119de1f3;
+        x = ((x >> 16) ^ x) * 0x119de1f3;
+        x = (x >> 16) ^ x;
+        x = Math.abs(x % tableSize);
+        return x;
     }
 
-    static int hash2(int n, int tableSize){
-        int hash = n*n % tableSize;
-        hash = hash << (tableSize / 27);
-        return hash;
+    static class ListElement {
+        ListElement next;
+        ListElement prev;
+        int data;
     }
 
-    static void insert(int n, String[] hashTable){
-        int t;
-        int h1 = hash1(n, hashTable.length);
-        int h2 = hash2(n, hashTable.length);
-        if (hashTable[h1].compareTo(Integer.toString(n)) != 0 && hashTable[h2].compareTo(Integer.toString(n)) != 0){
-            if(hashTable[h1].compareTo("null") == 0) hashTable[h1] = Integer.toString(n);
+    static class List{
+        ListElement head;
+        ListElement tail;
+
+        void insert(int value){
+            ListElement a = new ListElement();
+            a.data = value;
+            if (exists(value)) return;
+            if (tail == null){
+                head = a;
+                tail = a;
+            }
             else{
-                if(hashTable[h2].compareTo("null") != 0){
-                    t = Integer.parseInt(hashTable[h2]);
-                    hashTable[h2] = Integer.toString(n);
-                    hashTable[hash2(t, hashTable.length)] = Integer.toString(t);
+                tail.next = a;
+                a.prev =  tail;
+                tail = a;
+            }
+        }
+
+        void delete(int value){
+            if (head == null) return;
+            ListElement a;
+            a = head;
+            while (a != null){
+                if (a.data == value){
+                    if (a.next == null && a.prev == null){
+                        tail = null;
+                        head = null;
+                    }
+                    else {
+                        if (a.next == null){
+                            a.prev.next = null;
+                            tail = a.prev;
+                        }
+                        else {
+                            if (a.prev == null){
+                                a.next.prev = null;
+                                head = a.next;
+                            }
+                            else{
+                                a.next.prev = a.prev;
+                                a.prev.next = a.next;
+                            }
+                        }
+                    }
+                    return;
                 }
-                else hashTable[h2] = Integer.toString(n);
+                else {
+                    a = a.next;
+                }
             }
         }
-    }
 
-    static void delete(int n, String[] hashTable){
-        int h1 = hash1(n, hashTable.length);
-        int h2 = hash2(n, hashTable.length);
-        if (hashTable[h1].compareTo(Integer.toString(n)) == 0) hashTable[h1] = "null";
-        else{
-            if (hashTable[h2].compareTo(Integer.toString(n)) == 0) hashTable[h2] = "null";
+        boolean exists(int value){
+            ListElement a = head;
+            if (head == null) return false;
+            while (a != null){
+                if (a.data == value) return true;
+                a = a.next;
+            }
+            return false;
         }
     }
 
-    static void exists(int n, String[] hashTable){
-        int h1 = hash1(n, hashTable.length);
-        int h2 = hash2(n, hashTable.length);
-        if(hashTable[h1].compareTo(Integer.toString(n)) == 0 || hashTable[h2].compareTo(Integer.toString(n)) == 0) out.println("true");
-        else out.println("false");
-    }
+    public static void main(String[] args) throws IOException {
 
-   /* static  String[] reHash(String hashTable){
-        boolean overflow = true;
-        if(overflow){
-            String[] nHashTable = new String[hashTable.length()+ ( hashTable.length() / 2)];
-        }
-        else {
-            String[] nHashTable = new String[hashTable.length()];
-        }
-
-
-    }*/
-
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new FileInputStream("set.in"));
         out = new PrintWriter("set.out");
+        BufferedReader reader = new BufferedReader(new FileReader(new File("set.in")));
+        String line;
 
-        String[] hashTable = new String[1023];
-        for (int i = 0; i < hashTable.length; i++){
-            hashTable[i] = "null";
-        }
-
+        List[] hashTable = new List[1048576];
         String comand;
-        int n;
+        int value, h;
 
-        while (in.hasNext()){
-            comand = in.next();
-            if(comand.compareTo("insert") == 0){
-                n = in.nextInt();
-                insert(n,hashTable);
+        for(int i = 0; i < hashTable.length; i++) hashTable[i] = new List();
+
+        while ((line = reader.readLine()) != null){
+            String[] in = line.split(" ");
+            comand = in[0];
+            value = Integer.parseInt(in[1]);
+            h = hash(value, hashTable.length);
+
+            if (comand.compareTo("insert") == 0) {
+                hashTable[h].insert(value);
             }
 
-            if (comand.compareTo("delete") == 0){
-                n = in.nextInt();
-                delete(n,hashTable);
+            if (comand.compareTo("delete") == 0) {
+                hashTable[h].delete(value);
             }
 
-            if (comand.compareTo("exists") == 0){
-                n = in.nextInt();
-                exists(n, hashTable);
+            if (comand.compareTo("exists") == 0) {
+                if (hashTable[h].exists(value)) out.println("true");
+                else out.println("false");
             }
         }
 
-        in.close();
         out.close();
+        reader.close();
     }
 }
